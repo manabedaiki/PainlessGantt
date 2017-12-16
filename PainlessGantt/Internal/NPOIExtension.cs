e62d987e3b30fe0beedb78c48d3069bb46a2f670
manabedaiki <manabe.daiki@gmail.com>
2017-12-16 10:50:12 +0900

    ドキュメント

--- PainlessGantt/Internal/NPOIExtension.cs
+++ PainlessGantt/Internal/NPOIExtension.cs
@@ -4,8 +4,18 @@ using NPOI.SS.UserModel;
 
 namespace PainlessGantt.Internal
 {
+    /// <summary>
+    /// <see cref="NPOI"/> を拡張します。
+    /// </summary>
     internal static class NPOIExtension
     {
+        /// <summary>
+        /// セルに日付型の値を設定します。
+        /// 日付が規定値 (<c>default</c>) の場合は <c>null</c> を設定します。
+        /// </summary>
+        /// <param name="cell">値を設定する対象のセル。</param>
+        /// <param name="value">設定する日付。</param>
+        /// <exception cref="ArgumentNullException"><paramref name="cell"/> が <c>null</c> です。</exception>
         public static void SetCellValueOrDefault([NotNull] this ICell cell, DateTime value)
         {
             if (cell == null)
